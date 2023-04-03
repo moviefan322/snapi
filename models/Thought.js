3ace4cbf488332774ab49aa926b4bd6efaa86d4a
moviefan322 <philman202@yahoo.com>
2023-04-03 16:56:15 -0400

    sets up models and index

---
+++ models/Thought.js
@@ -0,0 +1,44 @@
+const { model, Schema } = require("mongoose");
+
+const reactionSchema = new Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    reactionBody: { type: String, required: true, maxlength: 280 },
+    username: { type: String, required: true },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (createdAtVal) => dateFormat(createdAtVal),
+    },
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+  }
+);
+
+const thoughtSchema = new Schema(
+  {
+    thoughtText: { type: String, required: true, minlength: 1, maxlength: 280 },
+    createdAt: { type: Date, default: Date.now },
+    username: { type: String, required: true },
+    reactios: [reactionSchema],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+  }
+);
+
+thoughtSchema.virtual("reactionCount").get(function () {
+  return this.reactions.length;
+});
+
+const Thought = model("Thought", thoughtSchema);
+
+module.exports = Thought;
