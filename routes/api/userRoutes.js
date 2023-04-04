b0a27ae9279ccb79bf6aa622c9e97874d2c3738f
moviefan322 <philman202@yahoo.com>
2023-04-03 21:33:59 -0400

    adds get user by ID route

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -24,6 +24,24 @@ router.get("/", async (req, res) => {
 
 //route to get a single user by its _id and populated thought and friend data
 
+router.get("/:id", async (req, res) => {
+  await User.findById({ _id: req.params.id })
+    .populate({
+      path: "thoughts",
+      select: "-__v",
+    })
+    .populate({
+      path: "friends",
+      select: "-__v",
+    })
+    .select("-__v")
+    .then((dbUserData) => res.json(dbUserData))
+    .catch((err) => {
+      console.log(err);
+      res.status(400).json(err);
+    });
+});
+
 //post a new user
 
 //put to update a user by its _id
